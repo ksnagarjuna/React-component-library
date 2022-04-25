14b381a664b4cc986d66386263075503f6c18d23
ksnagarjuna <snagarjuna@jord.com.au>
2022-04-25 10:15:36 +0530

    button files are changed

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -13,13 +13,13 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const HelloWorld = Template.bind({});
+export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-HelloWorld.args = {
-  label: 'Hello world!',
+Primary.args = {
+  label: 'Primary',
 };
 
-export const ClickMe = Template.bind({});
-ClickMe.args = {
-  label: 'Click me!',
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Secondary',
 };
