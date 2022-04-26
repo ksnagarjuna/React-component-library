40be69ba29bc6b68d90e4ea1c9cb6ef79913ad87
ksnagarjuna <snagarjuna@jord.com.au>
2022-04-26 14:22:29 +0530

    rollup.config.js file, package.json file and some styles are changed

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
-// import { terser } from "rollup-plugin-terser";
+ import postcss from "rollup-plugin-postcss";
+//  import { terser } from "rollup-plugin-terser";
  import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require("./package.json");
@@ -29,15 +29,14 @@ export default [
         resolve(),
         commonjs(),
         typescript({ tsconfig: "./tsconfig.json" }),
-        postcss(),
-        // terser(),
+         postcss(),
+        //  terser(),
       ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
-        external:[/\.css$/],
-    }
-
+         external:[/\.css$/],
+    },
 ];
